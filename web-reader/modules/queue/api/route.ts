ce68d83a7809e61c9f6e309b7fcac682eb2827c8
Jon Anthony <1292986+NoWayJA@users.noreply.github.com>
2024-12-22 14:54:57 +0000

    Queue API to fetch the oldest pending item from the Queue

--- web-reader/modules/queue/api/route.ts
+++ web-reader/modules/queue/api/route.ts
@@ -1,10 +1,30 @@
-/**
-* Dummy Route
- */
+import { db } from "@/db/db";
 
 export async function GET() {
-    return new Response("Example Placeholder response", {
+    const queue = await db.queue.findFirst({
+        include: {
+            url: {
+                include: {
+                    configuration: {
+                        include: {
+                            fields: {
+                                include: {
+                                    child: true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        where: { status: 'PENDING' },
+        orderBy: { actAfter: 'asc' }
+    });
+    if (!queue) {
+        return new Response('No pending items found', { status: 204 });
+    }
+    return new Response(JSON.stringify(queue), {
         status: 200,
-        headers: { 'Content-Type': 'text/plain' }
+        headers: { 'Content-Type': 'application/json' }
     });
 }
